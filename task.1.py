9569bd8c6e409958a300578a070e8229d93fad27
Bober028 <150669237+Bober028@users.noreply.github.com>
2024-03-02 21:07:42 +0200

    Add files via upload

--- task.1.py
+++ task.1.py
@@ -1,34 +1,38 @@
 import tkinter as tk
 
-def change_text():
-    button_clicks[0] += 1
-    if button_clicks[0] % 2 == 1:
-        label.config(text="Новий текст")
-    else:
-        label.config(text="Початковий текст")
-    update_title()
+def button_click(number):
+    current = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(0, str(current) + str(number))
 
-def update_title():
-    root.title(f"Зміна напису - Кількість клацань: {button_clicks[0]}")
+def button_clear():
+    entry.delete(0, tk.END)
+
+def button_equal():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(0, result)
+    except Exception as e:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Error")
 
 root = tk.Tk()
-root.title("Зміна напису")
-
-# Отримання розмірів екрану
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-
-# Встановлення розмірів вікна на весь екран
-root.geometry(f"{screen_width}x{screen_height}")
-
-label_text = "Початковий текст"
-label = tk.Label(root, text=label_text, font=("None", 16))
-label.pack(pady=20)
-
-button_clicks = [0]
-button = tk.Button(root, text="Змінити текст", command=change_text)
-button.pack()
-
-update_title()
-
+root.title("Калькулятор")
+entry = tk.Entry(root, width=16, borderwidth=5)
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', 'C', '=', '+'
+]
+row_val = 1
+col_val = 0
+for button in buttons:
+    tk.Button(root, text=button, padx=20, pady=20, command=lambda b=button: button_click(b) if b != '=' else button_equal() if b != 'C' else button_clear()).grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
 root.mainloop()
